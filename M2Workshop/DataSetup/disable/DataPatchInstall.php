ccea32d6bdadace0a8395ca0718a67e91f8a979d
Vyacheslav <vyacheslav.chuyan@decimadigital.com>
2019-04-04 20:10:48 +0300

    DB Schema

---
+++ M2Workshop/DataSetup/disable/DataPatchInstall.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace M2Workshop\DataSetup\Setup\Patch\Data;
+
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\SchemaPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+/**
+* Patch is mechanism, that allows to do atomic upgrade data changes
+*/
+class DataPatchInstall implements
+    DataPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface $moduleDataSetup
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     */
+    public function __construct(ModuleDataSetupInterface $moduleDataSetup)
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+    }
+
+    /**
+     * Do Upgrade
+     *
+     * @return void
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $data = [
+            ['name' => 'VolksWagen Passat', 'color'=>'black'],
+            ['name' => 'Peugeot', 'color'=>'red']
+        ];
+        foreach ($data as $bind) {
+            $this->moduleDataSetup->getConnection()
+                ->insertForce($this->moduleDataSetup->getTable('car'), $bind);
+        }
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [
+
+        ];
+    }
+}
---
+++ M2Workshop/DataSetup/disable/DataPatchUpdate.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace M2Workshop\DataSetup\Setup\Patch\Data;
+
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\SchemaPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+/**
+* Patch is mechanism, that allows to do atomic upgrade data changes
+*/
+class DataPatchUpdate implements
+    DataPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface $moduleDataSetup
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     */
+    public function __construct(ModuleDataSetupInterface $moduleDataSetup)
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+    }
+
+    /**
+     * Do Upgrade
+     *
+     * @return void
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $table = $this->moduleDataSetup->getTable('car');
+        $this->moduleDataSetup->getConnection()
+            ->insertForce($table, ['name' => 'Toyota Camry', 'color' => 'black']);
+
+        $this->moduleDataSetup->getConnection()
+            ->update($table, ['color' => 'red'], "name = 'Peugeot'");
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [
+            DataPatchInstall::class
+        ];
+    }
+}
